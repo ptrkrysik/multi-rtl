3426611769d0aa053316004db1c4aa5e838efb80
Piotr Krysik <ptrkrysik@gmail.com>
2017-01-01 22:12:37 +0100

    Remove try/except statements that cause that problems with loading multi_rtl_swig are not reported to the user.

--- python/__init__.py
+++ python/__init__.py
@@ -24,11 +24,7 @@ description here (python/__init__.py).
 '''
 
 # import swig generated symbols into the multi_rtl namespace
-try:
-	# this might fail if the module is python-only
-	from multi_rtl_swig import *
-except ImportError:
-	pass
+from multi_rtl_swig import *
 
 # import any pure python here
 from multi_rtl_source import multi_rtl_source
